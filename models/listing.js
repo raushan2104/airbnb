54b49e023f66f4d4c20b27cbdcf5c98cf7d5c16a
Raushan Raj <137853785+raushan2104@users.noreply.github.com>
2024-12-29 00:54:35 +0530

    create nav and footer

--- models/listing.js
+++ models/listing.js
@@ -12,7 +12,9 @@ const listingSchema = new Schema({
     default:
       "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
     set: (v) =>
-      v === ""
+      typeof v === "object" && v.url
+        ? v.url
+        : v === ""
         ? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
         : v,
   },
